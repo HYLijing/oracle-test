29e92968ff0bfa9b8fe27a416a2bf62fc6cf049e
yuanhaiyang <441001065@qq.com>
2020-09-29 17:02:15 +0800

    切换durid数据源

--- src/main/java/com/dongfang/test/config/DruidConfiguration.java
+++ src/main/java/com/dongfang/test/config/DruidConfiguration.java
@@ -1,11 +1,20 @@
 package com.dongfang.test.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class DruidConfiguration {
 
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DruidDataSource druid(){
+        return new DruidDataSource();
+    }
 
 }
