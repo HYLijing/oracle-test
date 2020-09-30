d8704fc85127e3d342da23521f76caeab52ed3de
yuanhaiyang <441001065@qq.com>
2020-09-30 13:39:33 +0800

    调整

--- src/main/java/com/dongfang/test/service/UserService.java
+++ src/main/java/com/dongfang/test/service/UserService.java
@@ -6,9 +6,13 @@ import com.dongfang.test.entity.User;
 import java.util.List;
 
 public interface UserService extends IService<User> {
-    int insertUser( User user );
-    int updateUser( User user );
-    int deleteUser( User user );
-    User findUserById( int id );
+    int insertUser(User user);
+
+    int updateUser(User user);
+
+    int deleteUser(User user);
+
+    User findUserById(int id);
+
     List<User> selectList(User user);
 }
